0e63d6c21c13ef58d7aacf0d34a0395e80e75476
thwee <thwee@storage.local>
2020-06-11 20:53:31 -0700

    Initial Import

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import FourD from '/js/vendor/fourd/fourd.js';
+
+var $fourd = new FourD();
+$fourd.init('#display', {
+  border: '1px solid 0x007bff',
+  width: window.innerWidth,
+  height: window.innerHeight,
+  background: 'rgba(1,1,1,0.5)'
+});
+
+$fourd.camera.position.z = -35;
+
+var h = $fourd.graph.add_vertex({
+  cube: {size: 10, color: 0x000000},
+  label: {size: 12, text: 'Hello'}
+})
+
+$fourd.toggle_controls('orbit', h)
+
+$fourd.make_resolve_click(function(vertex){
+  if(!vertex){
+    return;
+  }
+
+  $fourd.toggle_controls('orbit', vertex);
+})
+
+var g;
+setTimeout(function(){
+  g = $fourd.graph.add_vertex({
+    cube: {size: 10, color: 0x000000},
+    label: {size: 12, text: 'Graph!'}
+  });
+
+  $fourd.graph.add_edge(h, g, {color: 0xff614b, strength: 1.0});
+}, 1000);
+
+
